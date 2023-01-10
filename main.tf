4ac484db7e94ddf6b2808a1454407648656bdc81
esene.benjamin <eseneehis@yahoo.com>
2023-01-10 15:18:18 -0800

    Updated provider block

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ resource "aws_instance" "test" {
     Name = "TerraformEC2"
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
